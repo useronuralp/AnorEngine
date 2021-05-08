f4cb66078ed5bd0bd88296b77cece129cc02224c
Onur <onuralpsahin1@hotmail.com>
2021-05-08 20:02:28 +0300

    Included pch in imgui example builds

--- AnorEngine/src/ImGuiExampleBuilds/imgui_impl_opengl3.cpp
+++ AnorEngine/src/ImGuiExampleBuilds/imgui_impl_opengl3.cpp
@@ -79,7 +79,7 @@
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
-
+#include "pch.h"
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
